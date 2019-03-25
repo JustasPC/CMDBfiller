5f4faa6cf3ad38e35e38a53392d9c0dad295afd3
JustasPC <44752653+JustasPC@users.noreply.github.com>
2019-03-25 17:03:19 +0200

    Create fun_de.py

---
+++ fun_de.py
@@ -0,0 +1,94 @@
+net_tabs = {
+    "Network":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[12]/span[2]/a",
+    "Network1":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[1]/span[2]/a",
+    "Network2":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[2]/span[2]/a",
+    "Network3":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[3]/span[2]/a",
+    "Network4":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[4]/span[2]/a",
+    "Network5":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[5]/span[2]/a",
+    "Network6":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[6]/span[2]/a",
+    "Console":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[6]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[7]/span[2]/a"
+    }
+net_tabsv4 = {
+    "Network":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[12]/span[2]/a",
+    "Network1":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[1]/span[2]/a",
+    "Network2":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[2]/span[2]/a",
+    "Network3":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[3]/span[2]/a",
+    "Network4":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[4]/span[2]/a",
+    "Network5":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[5]/span[2]/a",
+    "Network6":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[6]/span[2]/a",
+    "Console":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/fieldset[12]/div[7]/div[2]/div[2]/div/dl/dd[7]/span[2]/a"
+    }
+
+cmdb_gui = {
+    "first_ci":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr[2]",
+    "ci_start":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr[",
+    "ci_end":"]/td[1]/nobr/span",
+    "result_cis":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr[3]/td[1]/nobr/span",
+    "second_ci":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[2]/div/div[2]/div/div[2]/table/tbody/tr[keiciama vieta]",
+    "res_numb":"/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[2]/div/div[1]/table/tbody/tr/td[2]",
+    }
+
+general = {
+    "location1":'//*[@id="arid_WIN_4_200000012"]',
+    "location2":'//*[@id="arid_WIN_4_200000007"]',
+    "location3":'//*[@id="arid_WIN_4_260000001"]',
+    "location4":'//*[@id="arid_WIN_4_260000004"]',
+    "location5":'//*[@id="arid_WIN_4_260000005"]',
+    "virtual":'//*[@id="arid_WIN_4_530054200"]',
+    }
+
+physical = {
+    'serial':'//*[@id="arid_WIN_4_200000001"]',
+    'is_virtual':'//*[@id="arid_WIN_4_530054200"]',
+    }
+
+specification = {
+    'model':'//*[@id="arid_WIN_4_700000003"]',
+    }
+tabs = {
+    'general':'/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[1]/span[2]/a',
+    'specification':'/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[2]/span[2]/a',
+    'backup':'/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[3]/span[2]/a',
+    'financials':'/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[11]/span[2]/a',
+    'network':'/html/body/div[1]/div[5]/div[2]/div/div/div[3]/fieldset/div/div/div/div/div[4]/fieldset/div/div/div/div[4]/div[6]/div/div/div[2]/fieldset/div/div[21]/div[2]/div[2]/div/dl/dd[12]/span[2]/a'
+    }
+
+backup = {
+    'type':'//*[@id="arid_WIN_4_536872027"]'
+    }
+financials = {
+    'details':'//*[@id="arid_WIN_4_536871935"]'
+    }
+
+browser = webdriver.Firefox()
+excelis = openpyxl.Workbook()
+url = 'https://arscmdb.telia.se/arsys/forms/arscmdb/SHR%3ALandingConsole/Default+Administrator+View/?timezone=Europe%2FHelsinki&cacheid=188fd2d0'
+
+
+def fun_decom_ci():
+    paste = pyperclip.paste()
+    text = [] 
+    text = paste.split('\t')
+    if len(text) <= 3:
+        print("Truksta")
+    else:
+        print("All ok")
+        text[2] = text[2][0:len(text[2])-2]
+    for key, value in tabs.items():
+        elem = browser.find_element_by_xpath(value)
+        elem.click()
+        if key=='general':
+            for x, y in general.items():
+                elem = browser.find_element_by_xpath(y)
+                print(elem.get_attribute('value'))
+        if key=='specification':
+            elem = browser.find_element_by_xpath(specification['model'])            
+            print(elem.get_attribute('value'))
+        if key=='backup':
+            elem = browser.find_element_by_xpath(backup['type'])            
+            print(elem.get_attribute('value'))
+        if key=='financials':
+            elem = browser.find_element_by_xpath(financials['details'])            
+            decom = "decom xxxx"
+            elem.send_keys(decom)
+        if 
